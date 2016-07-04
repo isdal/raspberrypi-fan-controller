87b3e97ac86df0c52e0f429a009932454cd72308
Tomas Isdal <tomas.isdal@gmail.com>
2016-07-03 21:47:06 -0700

    reorder start scrip to pull first

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+cd /home/isdal/raspberrypi-fan-controller
+git pull origin master
+echo "Restarting fan controller"
 pid=$(ps aux | grep fan_controller.py | grep -v 'grep')
 if [ -n "$VAR" ]; then
   echo "Killing old controller"
   kill `pgrep -f fan_controller.py`
 fi
-cd /home/isdal/raspberrypi-fan-controller
-git pull origin master
-echo "Restarting fan controller"
 PYTHONPATH='.' /usr/bin/python fancontroller/fan_controller.py --log=DEBUG
