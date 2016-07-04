0b1bf8d3446ebf8cefb9cea8595a5c7fde41dd96
Tomas Isdal <tomas.isdal@gmail.com>
2016-07-03 21:11:44 -0700

    update start script to kill old pid and git pull

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd ~/house_fan_controller/
+pid = $(ps aux | grep fan_controller.py | grep -v 'grep' | awk '{print $2}')
+if [ -n "$VAR" ]; then
+  echo "Killing old controller"
+  kill $pid
+fi
+cd /home/isdal/raspberrypi-fan-controller
+git pull origin master
+echo "Restarting fan controller"
 PYTHONPATH='.' /usr/bin/python fancontroller/fan_controller.py --log=DEBUG
