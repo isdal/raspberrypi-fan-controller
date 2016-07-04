623fbd1bc89e8920c7ac7d0fc37227273653d7b0
Tomas Isdal <tomas.isdal@gmail.com>
2016-07-03 21:25:22 -0700

    fix typo in pid

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid = $(ps aux | grep fan_controller.py | grep -v 'grep' | awk '{print $2}')
+pid=$(ps aux | grep fan_controller.py | grep -v 'grep' | awk '{print $2}')
 if [ -n "$VAR" ]; then
   echo "Killing old controller"
   kill $pid
