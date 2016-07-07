42dbf2d15ed0c5231239a95ae669c34515cc60c9
Tomas Isdal <tomas.isdal@gmail.com>
2016-07-07 10:32:58 -0700

    remove logging config override

--- fancontroller/fan_controller.py
+++ fancontroller/fan_controller.py
@@ -215,7 +215,6 @@ class MetricsUploader:
 
 if __name__ == '__main__':
     logging.basicConfig(
-        level=logging.INFO,
         format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
         datefmt="%H:%M:%S", stream=sys.stdout)
 
